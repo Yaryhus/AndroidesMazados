ef3f39a6ecaeaa82f8b4c8781a08b5f6c82defc5
Sergi <sergiwester>
2018-12-03 22:49:47 +0100

    Arreglada la rotación en POpcional

--- P_Opcional_SergioG_GuillermoA/Assets/Scripts/CamaraRotator.cs
+++ P_Opcional_SergioG_GuillermoA/Assets/Scripts/CamaraRotator.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamaraRotator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    Vector3 dir = Vector3.zero;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
 
-        transform.localEulerAngles = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+        // clamp acceleration vector to unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        transform.localEulerAngles = dir;
+     //  transform.localEulerAngles = new Vector3(-Input.acceleration.y, Input.acceleration.x, Input.acceleration.z);
     }
 }
